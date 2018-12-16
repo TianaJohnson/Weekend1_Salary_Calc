04c6ac423d241360faa81950c4533be0b50d1445
Tiana Johnson <Tiana.Marie013@gmail.com>
2018-12-15 20:28:07 -0600

    saving before I mess it up more

--- client.js
+++ client.js
@@ -1,59 +1,76 @@
  //...emplyees array...//
  let employees = [];
 
- //...employees class...//
-class Employee{
-    constructor( firstname, lastname, employID, empTitle, annualSalary ){
-        this.firstname = fistName;
-        this.lastname = lastName;
-        this.employID = employID;
-        this.empTitle = title;
-        this.annualSalary = annualSalary;
-    }
 
-}
 
-//...jQueary function setup...///
-$(document).ready(function(){
+//... jQueary function setup ...//... clicks ...//
+$(document).ready(function() {
      count = 0;
-
     $('#submit').on('click', submitClick );
     $('#submit').on('click', clearData );
-    $('#removeContent').on('click', );
+    $('#submit').on('click', clickAddName );
+    $('#submit').on('click', newEmp );
+    // $('#submit').on('click', updateInfo );
+           
 });
 
-// submit click function... console logs count
+ // //  ... employees class ...//
+class Employee{
+    constructor( firstname, lastname, employID, empTitle, annualSalary ) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.employID = employID;
+        this.empTitle = empTitle;
+        this.annualSalary = annualSalary;
+    }
+}
+
+//... submit click function ... console logs count ...// 
 function submitClick(){
     count += 1;
-    
     console.log('click', count );
-
-}
-// Clear data from input fields after click
-function clearData(){
-    $('#firstNameInput').val('');
-   // $('#lastNameInput').val('');
-    //$('#emplIDInput').val('');
-    //$('#empTitle').val('');
-    //$('#annualSalaryInput').val('');
 }
 
-// remove line from table, may not work, not ready to mess with
-// function removeCont(){
-//    $('#removeContent').remove('#contentAdd');
-// }
+    function clickAddName(){
+        console.log('info recieved');
+        let fName = $('.firstNameInput').val();
+        let lName = $('.lastNameInput').val();
+        let empId = $('.emplIDInput').val();
+        let empTitel = $('.empTitleInput').val();
+        let annSal = $('.annualSalaryInput').val();
 
-//new employee class function
-function newEmpInfo( firstname, lastname, employID, empTitle, annualSalary){
-    console.log( 'employee info:', firstname, lastname, employID, empTitle, annualSalary);
-    employees.push( new Employee( firstname, lastname, employID, empTitle, annualSalary ) );
-    return true;
-    } // end employee class function
-
- function updatInfo(){
-        let outputElement = $( '#contentAdd' );     
-        for( let employee of employees ){
-         $('#firstNameAddIn').append('<td>' + $firstNameInput + '</td>');
+        $('.firstNameAdd').append('<tr><td>' + '${fName}' + '</td></tr>' );
+        $('.lastNameAdd').append('<tr><td>${lName}</td></tr>');
+        $('.empIDAdd').append('<tr><td>${empId}</td></tr>');
+        $('.empTitleAdd').append('<tr><td>${empTitel}</td></tr>');
+        $('.annSalAdd').append('<tr><td>${annSal}</td></tr>');
        
-        }
-    }
+    }// end submit click function
+
+//... Clear data from input fields after click ...//
+function clearData(){
+    $('.firstNameInput').val(''),
+    $('.lastNameInput').val(''),
+    $('.emplIDInput').val(''),
+    $('.empTitleInput').val(''),
+    $('.annualSalaryInput').val('');
+}// end clear data function
+
+function newEmp( firstname, lastname, employID, empTitle, annualSalary ){
+    console.log( 'Employee:', firstname, lastname, employID, empTitle, annualSalary );
+    employees.push( new Employee ( firstname, lastname, employID, empTitle, annualSalary  ) );
+    
+}
+
+// function updateInfo(){
+//     let outputElement = $( '#contentAdd' );
+//     outputElement.empty();
+//     for( let employee of employees ){
+//       outputElement.append( '<tr><td>' + employee.firstname + '</td>',
+//       '<td>' + employee.lastname + '</td>',
+//       '<td>' + employee.employID + '</td>',
+//       '<td>' + employee.empTitle + '</td>',
+//       '<td>' + employee.annualSalary + '</td>',
+//       '<td><button ${#removeContent}>' + " X " + '</button> </td></tr>' );
+//     }
+// }
