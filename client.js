771a5deddff6224262af099c4bfba24db2721990
Tiana Johnson <Tiana.Marie013@gmail.com>
2018-12-16 10:59:26 -0600

    minor changes, testing functions, seeing what works, breaking up functions

--- client.js
+++ client.js
@@ -8,22 +8,23 @@ $(document).ready(function() {
      count = 0;
     $('#submit').on('click', submitClick );
     $('#submit').on('click', clearData );
-    $('#submit').on('click', clickAddName );
-    $('#submit').on('click', newEmp );
+    //$('#submit').on('click', clickAddName );
+    //$('#submit').on('click', newEmp );
     // $('#submit').on('click', updateInfo );
+    $('#submit').on('click', fNameClick );
            
 });
 
  // //  ... employees class ...//
-class Employee{
-    constructor( firstname, lastname, employID, empTitle, annualSalary ) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.employID = employID;
-        this.empTitle = empTitle;
-        this.annualSalary = annualSalary;
-    }
-}
+// class Employee{
+//     constructor( firstname, lastname, employID, empTitle, annualSalary ) {
+//         this.firstname = firstname;
+//         this.lastname = lastname;
+//         this.employID = employID;
+//         this.empTitle = empTitle;
+//         this.annualSalary = annualSalary;
+//     }
+// }
 
 //... submit click function ... console logs count ...// 
 function submitClick(){
@@ -31,21 +32,35 @@ function submitClick(){
     console.log('click', count );
 }
 
-    function clickAddName(){
-        console.log('info recieved');
-        let fName = $('.firstNameInput').val();
-        let lName = $('.lastNameInput').val();
-        let empId = $('.emplIDInput').val();
-        let empTitel = $('.empTitleInput').val();
-        let annSal = $('.annualSalaryInput').val();
+function fNameClick(){
+    console.log('still trying');
+    let fName = $('.firstNameInput').val();
+    let fNameAdd = $('.firstNameAdd');
+    fNameAdd.empty();
+    console.log ( fName ) ;
+    $(fName).append( fNameAdd );
 
-        $('.firstNameAdd').append('<tr><td>' + '${fName}' + '</td></tr>' );
-        $('.lastNameAdd').append('<tr><td>${lName}</td></tr>');
-        $('.empIDAdd').append('<tr><td>${empId}</td></tr>');
-        $('.empTitleAdd').append('<tr><td>${empTitel}</td></tr>');
-        $('.annSalAdd').append('<tr><td>${annSal}</td></tr>');
+}
+// function to append input info on the DOM
+    // function clickAddName(){
+    //     console.log('info recieved');
+    //     let fName = $('.firstNameInput').val();
+        // let lName = $('.lastNameInput').val();
+        // let empId = $('.emplIDInput').val();
+        // let empTitel = $('.empTitleInput').val();
+        // let annSal = $('.annualSalaryInput').val();
+
+        // $('.firstNameAdd').append('<tr><td>${fName}</td></tr>' );
+        // $('.lastNameAdd').append('<tr><td>${lName}</td></tr>');
+        // $('.empIDAdd').append('<tr><td>${empId}</td></tr>');
+        // $('.empTitleAdd').append('<tr><td>${empTitel}</td></tr>');
+        // $('.annSalAdd').append('<tr><td>${annSal}</td></tr>');
        
-    }// end submit click function
+    //}// end submit click function
+
+// function updateDom(){
+//     $('#contentAdd').append()
+// }
 
 //... Clear data from input fields after click ...//
 function clearData(){
@@ -56,11 +71,11 @@ function clearData(){
     $('.annualSalaryInput').val('');
 }// end clear data function
 
-function newEmp( firstname, lastname, employID, empTitle, annualSalary ){
-    console.log( 'Employee:', firstname, lastname, employID, empTitle, annualSalary );
-    employees.push( new Employee ( firstname, lastname, employID, empTitle, annualSalary  ) );
+// function newEmp( firstname, lastname, employID, empTitle, annualSalary ){
+//     console.log( 'Employee:', firstname, lastname, employID, empTitle, annualSalary );
+//     employees.push( new Employee ( firstname, lastname, employID, empTitle, annualSalary  ) );
     
-}
+//}
 
 // function updateInfo(){
 //     let outputElement = $( '#contentAdd' );
