3580baa22a731c1cea2e3f03d909d3d0f48d4889
Tiana Johnson <Tiana.Marie013@gmail.com>
2018-12-17 09:45:41 -0600

    forgot to devide my total by 12 and comment

--- client1.js
+++ client1.js
@@ -13,23 +13,28 @@ class Employee {
     } // end constructor
 } // end class
 
+// jQueary ready function
+// click action
 $(document).ready(function () {
     console.log('jQueary is ready!');
     $('#submit').on('click', userInput);
     $('#submit').on('click', updateExpensis);
 
+// function to make new Employee = user input
     function userInput() {
         let newEmp = new Employee($('.firstNameInput').val(),
             $('.lastNameInput').val(), $('.emplIDInput').val(),
             $('.empTitleInput').val(), $('.annualSalaryInput').val());
 
+// push user info
         employeeArray.push(newEmp);
     }
 
 });
 
+// function to update salary
 function updateExpensis() {
-    let totalSalary = 0;
+    //let totalSalary = 0;
     let outputElement = $('.tableBody');
     outputElement.empty();
     for (employee of employeeArray) {
@@ -38,17 +43,18 @@ function updateExpensis() {
             '</td><td>' + employee.jobTitle + '</td><td>' +
             '$' + Number(employee.annualSalary).toFixed(2) + '</td><td>' +
             '<button class="deleteEmployeeButton">Delete</button>' + '</td></tr>');
-        totalSalary += Number(employee.annualSalary);
-        console.log(totalSalary);
-        $('#monthlySalary').append('<h4> Monthly Salary: $' + totalSalary.toFixed(2) + '</h4>');
+        salary += Number(employee.annualSalary);
+        console.log(salary);
+       // $('#monthlySalary').append('<h4> Monthly Salary: $' + totalSalary.toFixed(2) + '</h4>');
 
     }
 
     // console.log(totalSalary);
     clearInput();
-    updateSalary(totalSalary);
+    updateSalary(salary);
 }
 
+// Clear inout fields after button click function
 function clearInput() {
     $('.firstNameInput').val('');
     $('.lastNameInput').val('');
@@ -58,9 +64,10 @@ function clearInput() {
     console.log('cleared input');
 }
 
-function updateSalary(allSalary) {
-    console.log('in Salary:', allSalary);
-    let monthlySalary = salary + allSalary;
+// update Salary function
+function updateSalary() {
+   // console.log('in Salary:', allSalary);
+    let monthlySalary = salary / 12 ;
     let monthSal = $('#monthlySalary');
     monthSal.empty();
     monthSal.append('<h2> Monthly Salary: $' + monthlySalary.toFixed(2) + '</h2>');
