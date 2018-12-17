b723279e3903ba7fb23b9dccac4ac3a5c4fd9624
Tiana Johnson <Tiana.Marie013@gmail.com>
2018-12-16 18:54:07 -0600

    finally got table to update

--- client.js
+++ client.js
@@ -1,197 +1,55 @@
- //...emplyees array...//
- let employees = [];
-
- // Employee Class constructor 
-  class Employee{
-    constructor( firstname, lastname, employID, empTitle, annualSalary ) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.employID = employID;
-        this.empTitle = empTitle;
-        this.annualSalary = annualSalary;
-    }
-}
-// create new employee from the class constructor and push into array
-function newEmp( firstname, lastname, employID, empTitle, annualSalary ){
-    console.log( 'Employee:', firstname, lastname, employID, empTitle, annualSalary );
-    employees.push( new Employee ( $('.firstNameInput'),
-    $('.lastNameInput'), $('.emplIDInput'),
-     $('.empTitleInput'), $('.annualSalaryInput')) );
-    console.log( employees );
-}
-
-// function clickAddName(){
-//          newEmp ( $('.firstNameInput'),
-//          $('.lastNameInput'), $('.emplIDInput'),
-//           $('.empTitleInput'), $('.annualSalaryInput') );
-// }
-
-//  //user input function
-//  function userInput() {
-//     let firstName = $('#firstName').val();
-//     let lastName = $('#lastName').val();
-//     let empId = $('#empId').val();
-//     let jobTitle = $('#jobTitle').val();
-//     let annualSalary = $('#annualSalary').val();
-
-    //add employee to array
-
-    // newEmp = [firstName, lastName, empId, jobTitle, annualSalary];
-    // employeeArray.push(newEmp);
-
-//}//end userInput function
-
-
-    //add new employee info to DOM
-    // $('#submit').on('click', clickAddName )
-    //     //monthlySalaryTotal () ;
-      
-    // });
-    
-    
-    
-    
-    
-    
-    
-    //end for loop
-//  class Employee{
-//     constructor( firstname, lastname, employID, empTitle, annualSalary ) {
-//         this.firstname = firstname;
-//         this.lastname = lastname;
-//         this.employID = employID;
-//         this.empTitle = empTitle;
-//         this.annualSalary = annualSalary;
-//     }
-// }
-
-//  function clickAddName(){
-//     let newEmp = new Employee ( $('.firstNameInput'),
-//      $('.lastNameInput'), $('.emplIDInput'),
-//       $('.empTitleInput'), $('.annualSalaryInput') ); 
-      
-
-//         employees.push( newEmp );
-//         console.log( employees );
-    
-//  }
-
 
 // //... jQueary function setup ...//... clicks ...//
 $(document).ready(function() {
+    console.log('jQueary is ready!')
      count = 0;
-     $('#submit').on('click', newEmp )
+     let employeeArray = [];
 
-
-//     $('#submit').on('click', submitClick );
-//     $('#submit').on('click', clearData );
-//     $('#submit').on('click', clickAddName );
-//     //$('#submit').on('click', newEmp );
-//     // $('#submit').on('click', updateInfo );
-//     //$('#submit').on('click', fNameClick );
-//     //$('#submit').on('click', newEmpInfo );
-    
- });
-// // function click counter
-// function submitClick(){
-//     count += 1;
-//     console.log('click', count );
-// }
-
-//  // //  ... employees class ...//
-// // class Employee{
-// //     constructor( firstname, lastname, employID, empTitle, annualSalary ) {
-// //         this.firstname = firstname;
-// //         this.lastname = lastname;
-// //         this.employID = employID;
-// //         this.empTitle = empTitle;
-// //         this.annualSalary = annualSalary;
-// //     }
-// // }
-
-
-
-
-
-
-
-// // function fNameClick(){
-// //     console.log('still trying');
-// //     let fName = $('.firstNameInput').val();
-// //     let fNameAdd = $('.firstNameAdd');
-// //     fNameAdd.empty();
-// //     console.log ( fName ) ;
-// //     $(fName).append( fNameAdd );
-
-// // }
-// // function to append input info on the DOM
-//     // function clickAddName(){
+// On Click actions
+     //$('#submit').on('click', userInput )
+     //$('#submit').on('click', clearInput)
+     $('#submit').on('click', toDom )
         
-//     //     console.log('info recieved');
-//     //     let fName = $('.firstNameInput').val();
-//     //     let lName = $('.lastNameInput').val();
-//     //     let empId = $('.emplIDInput').val();
-//     //     let empTitel = $('.empTitleInput').val();
-//     //     let annSal = $('.annualSalaryInput').val();
-    
- 
 
-//     //     $('.firstNameAdd').append('<tr><td>' + employees.firstname + '</td></tr>' );
-//     //     $('.lastNameAdd').append('<tr><td>' + employees.lastname + '</td></tr>');
-//     //     $('.empIDAdd').append('<tr><td>' + employee.employID + '</td></tr>');
-//     //     $('.empTitleAdd').append('<tr><td>' + employee.empTitle+ '</td></tr>');
-//     //     $('.annSalAdd').append('<tr><td>' + employee.annualSalary + '</td></tr>');
+// userInput function
+// to take input info and push it into an array for safe keeping
+     function userInput(){
+         
+        let firstName = $('.firstNameInput').val();
+        let lastName = $('.lastNameInput').val();
+        let empId = $('.emplIDInput').val();
+        let jobTitle = $('.empTitleInput').val();
+        let annualSalary = $('.annualSalaryInput').val();
+console.log(' user input data');
+        //add employee to array
+        newEmp = [firstName, lastName, empId, jobTitle, annualSalary];
+        employeeArray.push(newEmp);
+        console.log( newEmp );
+        console.log('new employee info:', employeeArray );
        
-//     // }// end submit click function
+     }//end userInput function
 
-// // function updateDom(){
-// //     $('#contentAdd').append()
-// // }
+//add new employee info to DOM
+     function toDom(){
+         userInput();
+         clearInput();
+        let outputText = '';
+        for(let i = 0; i < employeeArray.length; i++){
+        outputText = ("<tr><td>" + employeeArray[i][0] + "</td><td>" + employeeArray[i][1] + "</td><td class='id'>" + employeeArray[i][2] + "</td><td>" + employeeArray[i][3] + "</td><td>" +
+        employeeArray[i][4] + "</td><td>" + '<button class="deleteEmployeeButton">Delete</button>' + "</td></tr>");
+        console.log( 'should be on the dom' )
+        }
+        $('.tableBody').append( outputText );
+        console.log('okay, whats up?')
+    }
+    
+    function clearInput(){
+        $('.firstNameInput').val('');
+        $('.lastNameInput').val('');
+        $('.emplIDInput').val('');
+        $('.empTitleInput').val('');
+        $('.annualSalaryInput').val('');
+        console.log('cleared input');
+    }
+});
 
-// //... Clear data from input fields after click ...//
-// function clearData(){
-//     $('.firstNameInput').val(''),
-//     $('.lastNameInput').val(''),
-//     $('.emplIDInput').val(''),
-//     $('.empTitleInput').val(''),
-//     $('.annualSalaryInput').val('');
-// }// end clear data function
-
-// // function newEmp( firstname, lastname, employID, empTitle, annualSalary ){
-// //     console.log( 'Employee:', firstname, lastname, employID, empTitle, annualSalary );
-// //     employees.push( new Employee ( firstname, lastname, employID, empTitle, annualSalary  ) );
-// //     console.log( employees );
-// // }
-
-
-// // function updateInfo(){
-// //     let outputElement = $( '#contentAdd' );
-// //     outputElement.empty();
-// //     for( let employee of employees ){
-// //       outputElement.append( '<tr><td>' + employee.firstname + '</td>',
-// //       '<td>' + employee.lastname + '</td>',
-// //       '<td>' + employee.employID + '</td>',
-// //       '<td>' + employee.empTitle + '</td>',
-// //       '<td>' + employee.annualSalary + '</td>',
-// //       '<td><button ${#removeContent}>' + " X " + '</button> </td></tr>' );
-// //     }
-// // }
-
-// // function newEmpInfo (){
-// //     clickAddName();
-// //     newEmp( $('.firstNameInput').val(),
-// //  $('.lastNameInput').val(), $('.emplIDInput').val(),
-// //   $('.empTitleInput').val(),$('.annualSalaryInput').val());
-// // }
-
-
-
-
-
-
-// // console.log('info recieved');
-// // let fName = $('.firstNameInput').val();
-// // let elNam = $('.lastNameInput').val();
-// // let empId = $('.emplIDInput').val();
-// // let empTitel = $('.empTitleInput').val();
-// // let annSal = $('.annualSalaryInput').val();
---
+++ test.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    console.log('George the cat');
+  
+    var employeeArray = [];
+  
+    var userInput = function() {
+      var firstName = $('#firstName').val();
+      var lastName = $('#lastName').val();
+      var empId = $('#empId').val();
+      var jobTitle = $('#jobTitle').val();
+      var annualSalary = $('#annualSalary').val();
+      //add employee to array
+      newEmp = [firstName, lastName, empId, jobTitle, annualSalary];
+      employeeArray.push(newEmp);
+    };//end userInput function
+  
+    //add new employee info to DOM
+    $('#submitButton').on('click', function(){
+      userInput();
+      $('#empInfoForm').find('input').val('');//clear form after submit
+      var outputText = '';
+      for(var i = 0; i < employeeArray.length; i++){
+      outputText = ("<tr><td>" + employeeArray[i][0] + "</td><td>" + employeeArray[i][1] + "</td><td class='id'>" + employeeArray[i][2] + "</td><td>" + employeeArray[i][3] + "</td><td>" +
+      employeeArray[i][4] + "</td><td>" + '<button class="deleteEmployeeButton">Delete</button>' + "</td></tr>");
+  
+      monthlySalaryTotal();
+    }//end for loop
+  
+      $('#allEmployees').append(outputText);
+      $('#monthlySalary').html("Montly Salary Total: " + (monthlySalaryTotal().toLocaleString('en-US', { style: 'currency', currency: 'USD' })));
+    });//end submitButton onClick function
+  
+  
+    //function for monthly cost of salaries
+    var monthlySalaryTotal = function() {
+      var salariesTotal = 0;
+      for (var i = 0; i < employeeArray.length; i++) {
+          salariesTotal += Number(employeeArray[i][4]);
+      }
+      return salariesTotal / 12;
+    };//end monthlySalary
+  
+    $('#allEmployees').on('click', '.deleteEmployeeButton', function(){
+      $(this).parent().parent().remove();
+      var id = $(this).closest('tr').find('.id').html();
+      for (var i = 0; i < employeeArray.length; i++) {
+      if (employeeArray[i][2] === id) {
+        var index = employeeArray.indexOf(employeeArray[i]);
+        if (index > -1) {
+            employeeArray.splice(index, 1);
+          }
+        }
+      }//end for loop
+      //recalculate monthlySalaryTotal and update DOM
+      monthlySalaryTotal();
+      $('#monthlySalary').html("Montly Salary Total: " + (monthlySalaryTotal().toLocaleString('en-US', { style: 'currency', currency: 'USD' })));
+    });//end deleteEmployee onClick function
+  });//end doc ready
+  
+  
